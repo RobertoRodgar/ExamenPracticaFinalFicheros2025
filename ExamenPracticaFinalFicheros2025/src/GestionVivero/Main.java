df3147428ff7be5d2d96cdf47b681b37a37442c7
DAM <DAM@LAB-35-PC19>
2025-10-22 10:14:45 +0200

    Primer commit

---
+++ ExamenPracticaFinalFicheros2025/src/GestionVivero/Main.java
@@ -0,0 +1,10 @@
+package GestionVivero;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ ExamenPracticaFinalFicheros2025/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module ExamenPracticaFinalFicheros2025 {
+}
