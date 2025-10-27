a0cc12417bf83673ca07c224735577411c3c8225
DAM <DAM@LAB-35-PC19>
2025-10-27 10:17:16 +0100

    Añadir clases

---
+++ ExamenPracticaFinalFicheros2025/src/GestionVivero/Empleado.java
@@ -0,0 +1,8 @@
+package GestionVivero;
+	enum CARGO {VENDEDOR, GESTOR};
+public class Empleado {
+	private int identificacion;
+	private String nombre;
+	private String contraseña;
+	private CARGO cargo;
+}
