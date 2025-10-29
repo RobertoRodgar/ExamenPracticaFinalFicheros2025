5d54a75403f2ce5888dae6dbdfd0f421baa5f3d1
DAM <DAM@LAB-35-PC19>
2025-10-29 12:41:40 +0100

    Primera clase productiva

--- ExamenPracticaFinalFicheros2025/src/GestionVivero/Empleado.java
+++ ExamenPracticaFinalFicheros2025/src/GestionVivero/Empleado.java
@@ -5,4 +5,51 @@ public class Empleado {
 	private String nombre;
 	private String contraseña;
 	private CARGO cargo;
+	
+	public Empleado(int identificacion, String nombre, String contraseña, CARGO cargo) {
+		super();
+		this.identificacion = identificacion;
+		this.nombre = nombre;
+		this.contraseña = contraseña;
+		this.cargo = cargo;
+	}
+
+	public int getIdentificacion() {
+		return identificacion;
+	}
+
+	public void setIdentificacion(int identificacion) {
+		this.identificacion = identificacion;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getContraseña() {
+		return contraseña;
+	}
+
+	public void setContraseña(String contraseña) {
+		this.contraseña = contraseña;
+	}
+
+	public CARGO getCargo() {
+		return cargo;
+	}
+
+	public void setCargo(CARGO cargo) {
+		this.cargo = cargo;
+	}
+
+	@Override
+	public String toString() {
+		return "Empleado [identificacion=" + identificacion + ", nombre=" + nombre + ", contraseña=" + contraseña + ", cargo=" + cargo + "]";
+	}
+	
+	
 }
